322f044b3a1df5e0ec8f3796da8ad781485ba882
Godric <godolphine21@gmail.com>
2021-12-09 19:01:12 +0000

    Added implementation for websockets

---
+++ src/main/java/io/turntabl/super2/orderProcessingService/configurations/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package io.turntabl.super2.orderProcessingService.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/websocket")
+////                .setAllowedOrigins("http://127.0.0.1:5500");
+//                .setAllowedOriginPatterns("*");
+        registry.addEndpoint("/websocket")
+//                .setAllowedOrigins("http://127.0.0.1:5500")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
